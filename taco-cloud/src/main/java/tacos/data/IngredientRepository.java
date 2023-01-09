05b6e36f840d2df66fe050f40921a566d722386a
Plamen Tenev <plamen.tenev@accenture.com>
2023-01-09 16:38:13 +0200

    commit #3

--- taco-cloud/src/main/java/tacos/data/IngredientRepository.java
+++ taco-cloud/src/main/java/tacos/data/IngredientRepository.java
@@ -1,15 +1,10 @@
 package tacos.data;
 
+import org.springframework.data.repository.CrudRepository;
+
 import tacos.Ingredient;
 
-import java.util.Optional;
+public interface IngredientRepository
+        extends CrudRepository<Ingredient, String> {
 
-public interface IngredientRepository {
-
-  Iterable<Ingredient> findAll();
-  
-  Optional<Ingredient> findById(String id);
-  
-  Ingredient save(Ingredient ingredient);
-  
 }
